8f5ddaf8f4c720d74b2d6274e9af713f4e99fece
kiri11-mi1 <kirill.mil.0310@mail.ru>
2021-08-15 16:48:53 +0700

    объявил кверисет в экшене

--- server/apps/budget/views.py
+++ server/apps/budget/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.db.models import Sum
+from django.db.models import Sum, Value
+from django.db.models.functions import Coalesce
+from django.db.models import DecimalField
 
 from . import models
 from . import serializers
@@ -12,12 +14,16 @@ class CategoryView(viewsets.ModelViewSet):
     serializer_class = serializers.CategorySerializer
     queryset = models.Category.objects.all()
 
-    def get_queryset(self):
-        return models.Category.objects.filter(transaction__isnull=False).annotate(sum=Sum(('transaction__amount')))
-
     @action(detail=False, methods=['GET'])
     def get_all_categories_info(self, request):
-        serializer = self.get_serializer(self.get_queryset(), many=True)
+        all_categories = self.queryset.annotate(
+            amount=Coalesce(
+                Sum('transaction__amount'), 
+                Value(0),
+                output_field=DecimalField()
+            )
+        )
+        serializer = self.get_serializer(all_categories, many=True)
         return Response(serializer.data)
 
 
