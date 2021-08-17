fc0296e6d99841d009cba64fa44e85d4c000fde9
kiri11-mi1 <kirill.mil.0310@mail.ru>
2021-08-17 14:28:33 +0700

    #4 - Блок Global

--- server/apps/budget/views.py
+++ server/apps/budget/views.py
@@ -1,9 +1,10 @@
+import re
+from django.utils import translation
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.db.models import Sum, Value
+from django.db.models import Sum, Value, DecimalField, Q
 from django.db.models.functions import Coalesce
-from django.db.models import DecimalField
 
 from . import models
 from . import serializers
@@ -35,3 +36,25 @@ class TransactionViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.TransactionSerializer
     pagination_class = StandardResultsSetPagination
     queryset = models.Transaction.objects.all()
+
+    @action(detail=False, methods=['GET'])
+    def global_info(self, request):
+        amount_by_categories = self.queryset.aggregate(
+            income=Coalesce(
+                Sum(
+                    'amount', 
+                    filter=Q(category__type=0),
+                ), 
+                Value(0),
+                output_field=DecimalField(),
+            ),
+            expense=Coalesce(
+                Sum(
+                    'amount',
+                    filter=Q(category__type=1),
+                ),
+                Value(0),
+                output_field=DecimalField(),
+            )
+        )
+        return Response(amount_by_categories)
