592cab6eb07152118fd5ef2fed38166ab1d845f5
kiri11-mi1 <kirill.mil.0310@mail.ru>
2021-09-18 18:40:14 +0700

    убрал ненужную фикстуру

--- server/conftest.py
+++ server/conftest.py
@@ -36,18 +36,3 @@ def api_client_with_user(auth_data: dict) -> APIClient:
     client.force_authenticate(user, RefreshToken.for_user(user).access_token)
  
     return client
- 
- 
-@pytest.fixture
-def mock_file(file_name: str) -> bytes:
-    """
-    Fixture для тестирования методов, которые требуют какие-то файлы.
- 
-    Parameters:
-        file_name: название файла из папки mock_dat
- 
-    Returns:
-        Байты файла
-    """
-    with open(f'mock_data/{file_name}', 'rb') as file_io:
-        return file_io.read()
